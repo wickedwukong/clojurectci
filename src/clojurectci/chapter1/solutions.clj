f9c3b1622062ebcccef19226885954e884548b8f
Xuemin Guan <gxuemin@gmail.com>
2014-10-01 22:47:04 +0100

    adding comments to indicate exercise number
    which exercise each solution is for

--- src/clojurectci/chapter1/solutions.clj
+++ src/clojurectci/chapter1/solutions.clj
@@ -1,6 +1,7 @@
 (ns clojurectci.chapter1.solutions
   (:gen-class))
 
+;q1 detect whether a string contains only unqiue strings 
 (defn has_unique_chars [stringValue]
 	false)
 
--- test/clojurectci/chapter1/solutions_test.clj
+++ test/clojurectci/chapter1/solutions_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [clojurectci.chapter1.solutions :refer :all]))
 
+;q1 tests
 (deftest test_has_unique_chars
   (testing "has unique chars for various cases"
     (is (= true (has_unique_chars "a")))))
