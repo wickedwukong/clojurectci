f3eb33d445679d56e315da133449937d80d13262
Xuemin Guan <gxuemin@gmail.com>
2014-10-02 09:44:33 +0100

    using trampoline

--- src/clojurectci/chapter1/solutions.clj
+++ src/clojurectci/chapter1/solutions.clj
@@ -1,7 +1,20 @@
 (ns clojurectci.chapter1.solutions
   (:gen-class))
 
-;q1 detect whether a string contains only unqiue strings 
+(defn fa [stringValue stringSet] 
+	(if (= 0 (count stringValue)) true 
+		(if (contains? stringSet (.charAt stringValue 0))
+			false 
+			#(fb (subs stringValue 1) (conj stringSet (.charAt stringValue 0))))))
+
+(defn fb [stringValue stringSet] 
+	(if (= 0 (count stringValue)) true 
+		(if (contains? stringSet (.charAt stringValue 0))
+			false 
+			#(fb (subs stringValue 1) (conj stringSet (.charAt stringValue 0))))))
+
+
+;q1 detect whether a string contains only unique strings 
 (defn has_unique_chars [stringValue]
-	true)
+	(trampoline (fa stringValue #{})))
 
