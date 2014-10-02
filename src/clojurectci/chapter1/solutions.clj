e2cd4466d667a5b3a46a5a1663c8255a327ad88e
Xuemin Guan <gxuemin@gmail.com>
2014-10-03 00:33:00 +0100

    trying to fix the class cast exception

--- src/clojurectci/chapter1/solutions.clj
+++ src/clojurectci/chapter1/solutions.clj
@@ -4,13 +4,15 @@
 (declare has_unique_chars_a has_unique_chars_b)
 
 (defn has_unique_chars_a [str_value unique_char_set] 
-	(if (= 0 (count str_value)) true 
+	(if (= 0 (count str_value)) 
+		true
 		(if (contains? unique_char_set (.charAt str_value 0))
-			false 
+			false
 			#(has_unique_chars_b (subs str_value 1) (conj unique_char_set (.charAt str_value 0))))))
 
 (defn has_unique_chars_b [str_value unique_char_set] 
-	(if (= 0 (count str_value)) true 
+	(if (= 0 (count str_value)) 
+		true 
 		(if (contains? unique_char_set (.charAt str_value 0))
 			false 
 			#(has_unique_chars_a (subs str_value 1) (conj unique_char_set (.charAt str_value 0))))))
@@ -19,4 +21,6 @@
 ;q1 detect whether a string contains only unique strings 
 (defn has_unique_chars [str_value]
 	(trampoline (has_unique_chars_a str_value #{})))
+
+
 
