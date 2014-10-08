f400e0108e6cea027ecdcb05ceec9d095b2aea6f
Xuemin Guan <gxuemin@gmail.com>
2014-10-08 23:57:35 +0100

    more tests

--- src/clojurectci/chapter1/q2/reverse_string.clj
+++ src/clojurectci/chapter1/q2/reverse_string.clj
@@ -1,6 +1,14 @@
 (ns clojurectci.chapter1.q2.reverse-string
   (:gen-class))
 
-(defn reverse-string[value] value)
+(defn reverse-string[value] 
+	(if (= nil value)
+	nil
+	(.toString (.reverse (StringBuilder. value)))))
+
+(defn reverse-string-using-dotdot-macro[value] 
+	(if (= nil value)
+	nil
+	(.. (StringBuilder. value) reverse toString)))
 
 
--- test/clojurectci/chapter1/q2/reverse_string_test.clj
+++ test/clojurectci/chapter1/q2/reverse_string_test.clj
@@ -4,4 +4,16 @@
 
 (deftest test_reverse_string
   (testing "reverse various strings including empty and nil string"
-    (is (= "a" (reverse-string "a")))))
+  	(is (= "" (reverse-string "")))
+    (is (= "" (reverse-string-using-dotdot-macro "")))
+    (is (= " " (reverse-string " ")))
+    (is (= " " (reverse-string-using-dotdot-macro " ")))
+    (is (= "a" (reverse-string "a")))
+    (is (= "a" (reverse-string-using-dotdot-macro "a")))
+    (is (= "ba" (reverse-string "ab")))
+    (is (= "ba" (reverse-string-using-dotdot-macro "ab")))
+    (is (= "aa" (reverse-string "aa")))
+    (is (= "aa" (reverse-string-using-dotdot-macro "aa")))
+    (is (= nil (reverse-string nil)))
+    (is (= nil (reverse-string-using-dotdot-macro nil)))
+    ))
