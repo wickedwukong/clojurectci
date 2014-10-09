a26a5d7129826d0ed04856367a6493b9940ff9c6
Xuemin Guan <gxuemin@gmail.com>
2014-10-09 17:56:46 +0100

    using recur to reverse a string@

--- src/clojurectci/chapter1/q2/reverse_string.clj
+++ src/clojurectci/chapter1/q2/reverse_string.clj
@@ -6,6 +6,19 @@
 	nil
 	(.toString (.reverse (StringBuilder. value)))))
 
+(defn do-it[value, index, reversed] 
+	(if (>= index 0)
+		(recur value (- index 1) (.append reversed (.charAt value index)))
+		reversed
+		)
+	)
+
+(defn reverse-string-using-recur[value] 
+	(if (= nil value)
+	nil
+	(.toString (do-it value (- (.length  value) 1) (StringBuffer. "")))))
+
+
 (defn reverse-string-using-dotdot-macro[value] 
 	(if (= nil value)
 	nil
--- test/clojurectci/chapter1/q2/reverse_string_test.clj
+++ test/clojurectci/chapter1/q2/reverse_string_test.clj
@@ -6,10 +6,13 @@
   (testing "reverse various strings including empty and nil string"
   	(is (= "" (reverse-string "")))
     (is (= "" (reverse-string-using-dotdot-macro "")))
+    (is (= "" (reverse-string-using-recur "")))
     (is (= " " (reverse-string " ")))
     (is (= " " (reverse-string-using-dotdot-macro " ")))
+    (is (= " " (reverse-string-using-recur " ")))
     (is (= "a" (reverse-string "a")))
     (is (= "a" (reverse-string-using-dotdot-macro "a")))
+    (is (= "a" (reverse-string-using-recur "a")))
     (is (= "ba" (reverse-string "ab")))
     (is (= "ba" (reverse-string-using-dotdot-macro "ab")))
     (is (= "aa" (reverse-string "aa")))
