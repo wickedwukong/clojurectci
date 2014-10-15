f6b8d00f4c91459fbdf2fd94b3e638425af772eb
Xuemin Guan <gxuemin@gmail.com>
2014-10-15 23:14:03 +0100

    using built in count, first and rest

--- src/clojurectci/chapter1/q5/compress_string.clj
+++ src/clojurectci/chapter1/q5/compress_string.clj
@@ -6,19 +6,19 @@
 	))
 
 (defn empty-str [str-value]
-	(== (.length str-value) 0))
+	(== (count str-value) 0))
 
 (defn compress-recursively [compressed-string, current-char-count, remaining-string]
 	(if (empty-str remaining-string)
 		(do (append-current-compressed-string compressed-string current-char-count) 
 			(.toString compressed-string))
-		(let [next-char (.charAt remaining-string 0)
+		(let [next-char (first remaining-string)
 			  current-char (nth current-char-count 0)
 			  current-count (nth current-char-count 1)]
 			(if (not= current-char next-char)
 				(do (append-current-compressed-string compressed-string current-char-count)
-					(recur compressed-string [next-char 1] (.substring remaining-string 1)))
-				(recur compressed-string [current-char (+ current-count 1)] (.substring remaining-string 1))
+					(recur compressed-string [next-char 1] (rest remaining-string)))
+				(recur compressed-string [current-char (+ current-count 1)] (rest remaining-string))
 			))))
 
 
@@ -26,8 +26,8 @@
 (defn compress [str-to-be-compressed] 
 	(if (empty-str str-to-be-compressed)
 		str-to-be-compressed
-		(let [compressed (compress-recursively (StringBuffer. "") [(.charAt str-to-be-compressed 0) 1] (.substring str-to-be-compressed 1))]
-			(if (> (.length compressed) (.length str-to-be-compressed))
+		(let [compressed (compress-recursively (StringBuffer. "") [(first str-to-be-compressed) 1] (rest str-to-be-compressed))]
+			(if (> (count compressed) (count str-to-be-compressed))
 				str-to-be-compressed
 				compressed
 			))
