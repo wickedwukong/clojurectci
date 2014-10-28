5f8d6ad560283662a0618221961aebf7fc72dfe1
Xuemin Guan <gxuemin@gmail.com>
2014-10-28 23:21:58 +0000

    use union to concatenate two sets

--- src/clojurectci/chapter8/q5/parentheses.clj
+++ src/clojurectci/chapter8/q5/parentheses.clj
@@ -1,4 +1,5 @@
-(ns clojurectci.chapter8.q5.parentheses)
+(ns clojurectci.chapter8.q5.parentheses
+	(:require [clojure.set :refer [union]]))
 
 (defn- can_insert_left_paren [remaining_left_parens_count remaining_right_parens_count]
 	(> remaining_left_parens_count 0))
@@ -9,9 +10,9 @@
 
 (defn- find_all_properly_closed_parentheses [current_parentheses remaining_left_parens_count remaining_right_parens_count]
 		(cond (can_insert_left_paren remaining_left_parens_count remaining_right_parens_count)
-			        (conj #{} (find_all_properly_closed_parentheses (str current_parentheses "(") (- remaining_left_parens_count 1) remaining_right_parens_count))
+			        (union #{} (find_all_properly_closed_parentheses (str current_parentheses "(") (- remaining_left_parens_count 1) remaining_right_parens_count))
 		      (can_insert_right_paren remaining_left_parens_count remaining_right_parens_count)
-			        (conj #{} (find_all_properly_closed_parentheses (str current_parentheses ")") remaining_left_parens_count (- remaining_right_parens_count 1)))
+			        (union #{} (find_all_properly_closed_parentheses (str current_parentheses ")") remaining_left_parens_count (- remaining_right_parens_count 1)))
 			        :else #{})
 			)	
 
