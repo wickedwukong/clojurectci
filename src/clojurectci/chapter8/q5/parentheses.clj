7e3c373499b969fc8b84d78830b2729ce0c9470c
Xuemin Guan <gxuemin@gmail.com>
2014-10-28 09:47:35 +0000

    not working yet.

--- src/clojurectci/chapter8/q5/parentheses.clj
+++ src/clojurectci/chapter8/q5/parentheses.clj
@@ -1,10 +1,23 @@
 (ns clojurectci.chapter8.q5.parentheses)
 
-(defn- find_all_properly_closed_parentheses [current_parentheses remaining_left_parens_count remaining_right_parens_count closed_n_pair_of_parents]
-		#{"()"}	)
+(defn- can_insert_left_paren [remaining_left_parens_count remaining_right_parens_count]
+	(> remaining_left_parens_count 0))
+
+(defn- can_insert_right_paren [remaining_left_parens_count remaining_right_parens_count]
+	(> remaining_right_parens_count remaining_left_parens_count))
+
+
+(defn- find_all_properly_closed_parentheses [current_parentheses remaining_left_parens_count remaining_right_parens_count]
+		(cond (can_insert_left_paren remaining_left_parens_count remaining_right_parens_count)
+			        (conj #{} (find_all_properly_closed_parentheses (str current_parentheses "(") (- remaining_left_parens_count 1) remaining_right_parens_count))
+		      (can_insert_right_paren remaining_left_parens_count remaining_right_parens_count)
+			        (conj #{} (find_all_properly_closed_parentheses (str current_parentheses ")") remaining_left_parens_count (- remaining_right_parens_count 1)))
+			        :else #{})
+			)	
+
 
 (defn pair_of_parentheses [n]
 	(if (= n 0) 
 		#{}
-		(find_all_properly_closed_parentheses "(" (- n 1) n #{})
+		(find_all_properly_closed_parentheses "(" (- n 1) n)
 		))
