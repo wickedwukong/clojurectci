ed46392bdc8f61d76fae7cf3b9f986d2348d315f
Xuemin Guan <gxuemin@gmail.com>
2014-10-30 10:57:17 +0000

    a bit reformatting to make it more readable
    will look more closely to see if the code can be made better.

--- src/clojurectci/chapter8/q5/parentheses.clj
+++ src/clojurectci/chapter8/q5/parentheses.clj
@@ -9,19 +9,19 @@
 
 
 (defn- find_all_properly_closed_parentheses [current_parentheses remaining_left_parens_count remaining_right_parens_count closed_paren_set]
-	(union (if (can_insert_left_paren remaining_left_parens_count remaining_right_parens_count)
+	(union 
+		(if (can_insert_left_paren remaining_left_parens_count remaining_right_parens_count)
 		        (union closed_paren_set (find_all_properly_closed_parentheses (str current_parentheses "(") (- remaining_left_parens_count 1) remaining_right_parens_count closed_paren_set))
 		         closed_paren_set)
 
-	(if (can_insert_right_paren remaining_left_parens_count remaining_right_parens_count)
+		(if (can_insert_right_paren remaining_left_parens_count remaining_right_parens_count)
 		        (union closed_paren_set (find_all_properly_closed_parentheses (str current_parentheses ")") remaining_left_parens_count (- remaining_right_parens_count 1) closed_paren_set))
 		        closed_paren_set)
      
-    (if (and (= remaining_right_parens_count 0) (= remaining_left_parens_count 0))
-       (conj closed_paren_set current_parentheses)
-       closed_paren_set)
-    )  
-    ; (println (str "==" current_parentheses "=="))
+	    (if (and (= remaining_right_parens_count 0) (= remaining_left_parens_count 0))
+		       (conj closed_paren_set current_parentheses)
+		       closed_paren_set)
+	  )  
 )	
 
 
