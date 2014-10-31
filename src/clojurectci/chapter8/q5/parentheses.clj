332e01442265f9e631ff21b114c4f68b0ed9c5e5
Xuemin Guan <gxuemin@gmail.com>
2014-10-31 00:13:52 +0000

    a bit of renaming to make it shorter.

--- src/clojurectci/chapter8/q5/parentheses.clj
+++ src/clojurectci/chapter8/q5/parentheses.clj
@@ -1,20 +1,20 @@
 (ns clojurectci.chapter8.q5.parentheses
 	(:require [clojure.set :refer [union]]))
 
-(defn- can_insert_left_paren [remaining_left_parens_count remaining_right_parens_count]
+(defn- left_parent? [remaining_left_parens_count remaining_right_parens_count]
 	(> remaining_left_parens_count 0))
 
-(defn- can_insert_right_paren [remaining_left_parens_count remaining_right_parens_count]
+(defn- right_parent? [remaining_left_parens_count remaining_right_parens_count]
 	(and (> remaining_right_parens_count remaining_left_parens_count) (>= remaining_left_parens_count 0)))
 
 
 (defn- find_all_properly_closed_parentheses [current_parentheses remaining_left_parens_count remaining_right_parens_count closed_paren_set]
 	(union 
-		(if (can_insert_left_paren remaining_left_parens_count remaining_right_parens_count)
+		(if (left_parent? remaining_left_parens_count remaining_right_parens_count)
 		        (union closed_paren_set (find_all_properly_closed_parentheses (str current_parentheses "(") (- remaining_left_parens_count 1) remaining_right_parens_count closed_paren_set))
 		         closed_paren_set)
 
-		(if (can_insert_right_paren remaining_left_parens_count remaining_right_parens_count)
+		(if (right_parent? remaining_left_parens_count remaining_right_parens_count)
 		        (union closed_paren_set (find_all_properly_closed_parentheses (str current_parentheses ")") remaining_left_parens_count (- remaining_right_parens_count 1) closed_paren_set))
 		        closed_paren_set)
      
