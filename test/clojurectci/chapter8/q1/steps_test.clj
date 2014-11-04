bc717d012ed1a05f978aa9c0a997cb72d1d42ba4
Xuemin Guan <gxuemin@gmail.com>
2014-11-04 00:19:13 +0000

    adding more tests.

--- test/clojurectci/chapter8/q1/steps_test.clj
+++ test/clojurectci/chapter8/q1/steps_test.clj
@@ -4,6 +4,9 @@
 
 (deftest test_steps
   (testing "different steps to climb a n step stair"
+  	(is (= #{} (steps 0)))
   	(is (= #{`(1)} (steps 1)))
   	(is (= #{`(1 1) `(2)} (steps 2)))
+  	(is (= #{`(1 1 1) `(1 2) `(2 1) `(3)} (steps 3)))
+  	(is (= #{`(1 1 1 1) `(1 1 2) `(1 2 1) `(2 1 1) `(2 2) `(1 3) `(3 1)} (steps 4)))
  )) 
