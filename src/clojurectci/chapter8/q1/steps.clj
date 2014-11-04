c25bda7f4397235348bc33e3fee388a6bf15b20b
Xuemin Guan <gxuemin@gmail.com>
2014-11-04 00:07:45 +0000

    making tests passing

--- src/clojurectci/chapter8/q1/steps.clj
+++ src/clojurectci/chapter8/q1/steps.clj
@@ -1,2 +1,35 @@
-(ns clojurectci.chapter8.q1.steps)
+(ns clojurectci.chapter8.q1.steps
+	(:require [clojure.set :refer [union]]))
+
+(defn- take_one_step [current_steps] (conj current_steps 1))
+
+(defn- take_two_steps [current_steps] (conj current_steps 2))
+
+(defn- take_three_steps [current_steps] (conj current_steps 3))
+
+(defn- not_enough_steps? [current_steps num_stairs]
+	(< (reduce + current_steps) num_stairs))
+
+(defn- just_enough_steps? [current_steps num_stairs]
+	(= (reduce + current_steps) num_stairs))
+
+(defn- find_steps [num_stairs current_steps step_set] 
+	(union
+		(if (just_enough_steps? current_steps num_stairs)
+	     	(conj step_set current_steps))
+
+		(if (not_enough_steps? current_steps num_stairs) 
+			(union step_set
+				(find_steps num_stairs (take_one_step current_steps) step_set)
+				(find_steps num_stairs (take_two_steps current_steps) step_set)
+				(find_steps num_stairs (take_three_steps current_steps) step_set)
+		    ))
+
+	    step_set) 	
+	)
+
+(defn steps [n]
+	(if (<= n 0)
+		#{}
+		(find_steps n () #{})))
 
--- test/clojurectci/chapter8/q1/steps_test.clj
+++ test/clojurectci/chapter8/q1/steps_test.clj
@@ -2,3 +2,8 @@
   (:require [clojure.test :refer :all]
             [clojurectci.chapter8.q1.steps :refer :all]))
 
+(deftest test_steps
+  (testing "different steps to climb a n step stair"
+  	(is (= #{`(1)} (steps 1)))
+  	(is (= #{`(1 1) `(2)} (steps 2)))
+ )) 
