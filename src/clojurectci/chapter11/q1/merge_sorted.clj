5bbe84a74aaf5f41f57289d440e7d6d3d3941436
Xuemin Guan <gxuemin@gmail.com>
2014-11-14 15:34:08 +0000

    some refactoring to make function smaller

--- src/clojurectci/chapter11/q1/merge_sorted.clj
+++ src/clojurectci/chapter11/q1/merge_sorted.clj
@@ -1,9 +1,12 @@
 (ns clojurectci.chapter11.q1.merge-sorted)
 
+(defn- add_remaing [merged_results current remaining]
+	(concat (conj merged_results current) remaining))
+
 (defn-  merge_in_ascending_order [current_a remaining_a current_b remaining_b merged_results]
 	(cond 
-		(nil? current_a) (concat (conj merged_results current_b) remaining_b)
-		(nil? current_b) (concat (conj merged_results current_a) remaining_a)
+		(nil? current_a) (add_remaing merged_results current_b remaining_b)
+		(nil? current_b) (add_remaing merged_results current_a remaining_a)
 		:else	(if (> current_a current_b) 
 					(recur (first remaining_a) (rest remaining_a) current_b remaining_b (conj merged_results current_a))
 					(recur current_a remaining_a (first remaining_b) (rest remaining_b) (conj merged_results current_b)))))
