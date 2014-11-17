f5ab57f094766b73cbe45ce29f64e037c6b4b0c4
Xuemin Guan <gxuemin@gmail.com>
2014-11-17 23:49:10 +0000

    adding a failing test and a skeleton implementation of finding neighbors for a 2d matrix cell

---
+++ src/joyofclojure/matrix_2d_neighbors.clj
@@ -0,0 +1,3 @@
+(ns joyofclojure.matrix-2d-neighbors)
+
+(defn neighbors [size yx] (list yx))
---
+++ test/joyofclojure/matrix_2d_neighbors_test.clj
@@ -0,0 +1,9 @@
+(ns joyofclojure.matrix-2d-neighbors-test
+  (:require [clojure.test :refer :all]
+            [joyofclojure.matrix-2d-neighbors :refer :all]))
+
+(deftest test_neighbors
+  (testing "finding neighboring celss in a 2D matrix"
+  	(is (= `([1 0] [0 1]) (neighbors 3 [0 0]))))
+  )
+  
