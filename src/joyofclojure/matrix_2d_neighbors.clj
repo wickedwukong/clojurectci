54f7e614e918ab27da8a4ca39afb7044ee91c665
Xuemin Guan <gxuemin@gmail.com>
2014-11-23 22:01:38 +0000

    implementing neighbors

--- src/joyofclojure/matrix_2d_neighbors.clj
+++ src/joyofclojure/matrix_2d_neighbors.clj
@@ -1,3 +1,9 @@
 (ns joyofclojure.matrix-2d-neighbors)
 
-(defn neighbors [size yx] (list yx))
+(defn neighbors 
+	([size yx] (neighbors [[-1 0] [1 0] [0 -1] [0 1]] size yx))
+	([delta size yx]
+		(filter (fn [new-yx] 
+			      (every? #(< -1 % size) new-yx))
+			(map #(vec (map + % yx)) delta))))
+
--- test/joyofclojure/matrix_2d_neighbors_test.clj
+++ test/joyofclojure/matrix_2d_neighbors_test.clj
@@ -4,6 +4,7 @@
 
 (deftest test_neighbors
   (testing "finding neighboring celss in a 2D matrix"
-  	(is (= `([1 0] [0 1]) (neighbors 3 [0 0]))))
+  	(is (= `([1 0] [0 1]) (neighbors 3 [0 0])))
+  	(is (= `([0 1] [2 1] [1 0] [1 2]) (neighbors 3 [1 1]))))
   )
   
