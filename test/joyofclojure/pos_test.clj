8183a40241e6694cfda9171e1b986eba930d7a08
Xuemin Guan <gxuemin@gmail.com>
2015-01-22 16:01:05 +0000

    test a list to make sure it works for generic sequence collection

--- test/joyofclojure/pos_test.clj
+++ test/joyofclojure/pos_test.clj
@@ -4,6 +4,7 @@
 
 (deftest test_pos
   (testing "finding the index or key"
+  	(is (= 5 (pos 3 '(:a 1 :b 2 :c 3 :d 5))))
   	(is (= 5 (pos 3 [:a 1 :b 2 :c 3 :d 5])))
   	(is (= 2 (pos 3 [1 2 3])))
   	(is (= 0 (pos 1 [1 2 3])))
