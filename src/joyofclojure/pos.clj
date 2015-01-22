7900d2a4547019ba88997e573eeb3f6840a21a98
Xuemin Guan <gxuemin@gmail.com>
2015-01-22 16:47:34 +0000

    making it work for both sequence and map

--- src/joyofclojure/pos.clj
+++ src/joyofclojure/pos.clj
@@ -1,10 +1,19 @@
 (ns joyofclojure.pos)
 
-(defn pos ([e coll] (pos -1 e coll))
-	([index e coll] 
-		(cond 
-			(empty? coll) nil
-		    (= e (first coll)) (+ index 1)
-			:else (recur (+ index 1) e (rest coll)))))
+; (defn pos ([e coll] (pos -1 e coll))
+	; ([index e coll] 
+		; (cond 
+			; (empty? coll) nil
+		    ; (= e (first coll)) (+ index 1)
+			; :else (recur (+ index 1) e (rest coll)))))
 
+(defn find-key-or-index [e indexed-pairs]
+	(cond 
+		(empty? indexed-pairs) nil
+		(= e (second (first indexed-pairs))) (first (first indexed-pairs))
+		:else (recur e (rest indexed-pairs))))
 
+(defn pos [e coll]
+	(if (map? coll)
+		(find-key-or-index e (vec coll))
+		(find-key-or-index e (map-indexed vector coll))))
